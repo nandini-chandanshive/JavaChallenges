b30d8ec3b76a42d6ef169b2670bdda791e4f4435
nandini-chandanshive <nandinichandanshive@gmail.com>
2026-05-26 16:32:41 +0530

    added basicjavachallenges

---
+++ src/LeftHalfPyramid.java
@@ -0,0 +1,11 @@
+import java.lang.*;
+
+public class LeftHalfPyramid{
+    public static void main(String[] args) {
+        System.out.println("    *");
+        System.out.println("   **");
+        System.out.println("  ***");
+        System.out.println(" ****");
+        System.out.println("*****");
+    }
+}
---
+++ src/ReverseLeftHalfPyramid.java
@@ -0,0 +1,11 @@
+import java.lang.*;
+
+public class ReverseLeftHalfPyramid{
+    public static void main(String[] args) {
+        System.out.println("*****");
+        System.out.println(" ****");
+        System.out.println("  ***");
+        System.out.println("   **");
+        System.out.println("    *");
+    }
+}
